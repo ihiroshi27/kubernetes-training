cf0cf1bb32bedd4236fd70a4e7be22920893748d
Perayuth Thanyanon <com.ihiroshi.mbp1517@Hiroshis-MacBook-Pro-15.local>
2020-06-11 21:16:09 +0700

    add log listen and request

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.enable('trust proxy')
+
 app.get('/', (req, res, next) => {
-  res.send("Hello from NodeJS")
+  console.log(`${new Date().toISOString()} ${req.ip} ${req.method} ${req.path}`)
+  res.send("Hello, Hiroshi")
 })
 
-app.listen(8370)
+app.listen(8370, '0.0.0.0', () => {
+  console.info('Server is running at 8370')
+})
